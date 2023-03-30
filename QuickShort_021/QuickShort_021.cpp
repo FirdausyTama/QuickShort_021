3d33958f49c0a72af6352e73e191b8e425d4d61d
FirdausyTama <atama.firdausy@gmail.com>
2023-03-30 10:40:51 +0700

    perubahan nambah tanda ;

--- QuickShort_021/QuickShort_021.cpp
+++ QuickShort_021/QuickShort_021.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 //array of intergers to hold values
-int arr[20]
+int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang elemnt array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+}
